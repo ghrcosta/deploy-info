83f91c1d2d8a073c7991090428ce876edd220254
ghrcosta <geohrc.dev@gmail.com>
2025-12-20 20:25:45 -0300

    Convert to Gradle version catalog, update backend libraries

--- portal/deploy-info-portal/build.gradle.kts
+++ portal/deploy-info-portal/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.6"
-	id("io.spring.dependency-management") version "1.1.7"
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependencyManagement)
 }
 
 group = "io.github.ghrcosta"
@@ -18,40 +18,30 @@ repositories {
 	mavenCentral()
 }
 
-// See: https://javadoc.io/doc/org.mockito/mockito-core/latest/org.mockito/org/mockito/Mockito.html#0.3
-val mockitoAgent = configurations.create("mockitoAgent")
+// https://javadoc.io/doc/org.mockito/mockito-core/latest/org.mockito/org/mockito/Mockito.html#0.3
+val mockitoAgent: Configuration = configurations.create("mockitoAgent")
 
-extra["springCloudGcpVersion"] = "6.2.1"
-extra["springCloudVersion"] = "2024.0.1"
-val mockitoVersion = "5.14.2"
-val mockitoKotlinVersion = "5.4.0" // https://github.com/mockito/mockito-kotlin/releases
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(libs.kotlin.reflect)
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation(platform(libs.spring.cloud.bom))
+	implementation(libs.spring.boot.starter.web)
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation(libs.fasterxml.jackson)
 
-	implementation("com.google.cloud:spring-cloud-gcp-starter")
-	implementation("com.google.cloud:spring-cloud-gcp-starter-logging")
-	implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
-	implementation("com.google.cloud:spring-cloud-gcp-starter-data-datastore")
+	implementation(platform(libs.spring.cloud.gcp.bom))
+	implementation(libs.spring.cloud.gcp.starter)
+	implementation(libs.spring.cloud.gcp.starter.datastore)
+	implementation(libs.spring.cloud.gcp.starter.logging)
+	implementation(libs.spring.cloud.gcp.starter.storage)
 
+	testImplementation(libs.spring.boot.starter.test)
+	testImplementation(libs.junit)
+	testImplementation(libs.mockito.core)
+	testImplementation(libs.mockito.kotlin)
+	mockitoAgent(libs.mockito.core) { isTransitive = false }
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.mockito:mockito-core:${mockitoVersion}")
-	testImplementation("org.mockito.kotlin:mockito-kotlin:${mockitoKotlinVersion}")
-	mockitoAgent("org.mockito:mockito-core:${mockitoVersion}") { isTransitive = false }
-
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("com.google.cloud:spring-cloud-gcp-dependencies:${property("springCloudGcpVersion")}")
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+	testRuntimeOnly(libs.junit.launcher)
 }
 
 kotlin {
@@ -63,7 +53,6 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
 tasks.test {
 	jvmArgs("-javaagent:${mockitoAgent.asPath}")
 }
